d0a404bbb561b9e258c4697ec48b4a94b828a505
Kevin Cook <kevinac4@gmail.com>
2015-10-08 17:39:35 -0700

    fixing settings

--- back/untitled/settings.py
+++ back/untitled/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import datetime
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -106,7 +107,7 @@ REST_FRAMEWORK = {
 }
 
 JWT_AUTH = {
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3000),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=30),
 }
 
 # Internationalization
