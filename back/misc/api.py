9e6929c835c54dfc0e4e9f470e8da882b32a0e19
Kevin Cook <kevinac4@gmail.com>
2015-12-03 12:35:22 -0800

    better error checking on imagehelper endpoint

--- back/misc/api.py
+++ back/misc/api.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-
+from pprint import pprint
 class UserRegistration(object):
     def create(self, validated_data):
         user = User(email=validated_data['email'], username=validated_data['username'])
@@ -20,5 +20,8 @@ class ImageHelpers(APIView):
         except urllib.error.URLError:
             return Response({'image' : 'Url Does not Exist'}, status=status.HTTP_404_NOT_FOUND)
         string = html.read().decode('utf-8')
-        imgUrl = re.search('"og:image" content="(.+)"', string, re.IGNORECASE).group(1)
-        return Response({'image' : imgUrl}, status=status.HTTP_200_OK)
+        search = re.search('"og:imeage" content="(.+)"', string, re.IGNORECASE)
+        if search is not None:
+            imgUrl = search.group(1)
+            return Response({'image' : imgUrl}, status=status.HTTP_200_OK)
+        return Response({'image' : 'og:image tag not detected'}, status=status.HTTP_404_NOT_FOUND)
