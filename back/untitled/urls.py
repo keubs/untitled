a29edaa9352e3439d2e6445a0dccd9b548820221
Kevin Cook <kevinac4@gmail.com>
2015-12-11 08:06:07 -0800

    page by tags new url

--- back/untitled/urls.py
+++ back/untitled/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
 
     # Topics
     url(r'^api/topics/$', topic_api.TopicList.as_view()),
-    url(r'^api/topics/(?P<tag>[a-z]+)/$', topic_api.TopicListByTag.as_view()),
+    url(r'^api/topics/tag/(?P<tag>.*)/$', topic_api.TopicListByTag.as_view()),
     url(r'^api/topics/submit$', topic_api.TopicPost.as_view()),
     url(r'^api/topics/(?P<pk>[0-9]+)/$', topic_api.TopicDetail.as_view()),
     url(r'^api/token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
