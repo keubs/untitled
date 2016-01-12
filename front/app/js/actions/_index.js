6d99e7af6925bbe050d812f8daec57b75e46f75a
Kevin Cook <kevinac4@gmail.com>
2016-01-11 17:04:39 -0800

    adding actions working again

--- front/app/js/actions/_index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const angular = require('angular');
-
-exports = angular.module('topics', [])
-            .controller('TopicsCtrl', require('./topics-controller'))
-            .controller('TopicSubmitCtrl', require('./topic-submit-controller'))
-            .controller('TopicCtrl', require('./topic-controller')) // Singular
-            .service('TopicService', require('./topics-service'));
--- front/app/js/topics/_index.js
+++ front/app/js/topics/_index.js
@@ -6,4 +6,6 @@ exports = angular.module('topics', [])
             .controller('TopicsCtrl', require('./topics-controller'))
             .controller('TopicSubmitCtrl', require('./topic-submit-controller'))
             .controller('TopicCtrl', require('./topic-controller')) // Singular
+            .controller('ActionCtrl', require('./action-controller'))
+            .service('ActionService', require('./action-service'))
             .service('TopicService', require('./topics-service'));
