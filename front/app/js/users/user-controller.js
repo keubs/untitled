41b1343ab507e6359697562cc04e70b2513c8f24
Kevin Cook <kevinac4@gmail.com>
2016-07-19 10:19:34 -0700

    continuing work on user page

--- front/app/js/users/user-controller.js
+++ front/app/js/users/user-controller.js
@@ -1,13 +1,13 @@
 'use strict';
 
-module.exports = function($scope, $location, UserService, $auth, $http, $window, AppSettings, $stateParams) {
-
-
+module.exports = function($scope, $location, UserService, $auth, $http, AppSettings, $stateParams, AuthService, $rootScope) {
 	$scope.user = UserService.get($stateParams.userid)
 		.then(function(data){
 			$scope.user = data;
-			console.log($scope.user);
 		}, function(error){
 			console.log(error);
-		})
+		});
+
+	console.log($rootScope.user);
+	$scope.isCurrentUser = false;
 };
