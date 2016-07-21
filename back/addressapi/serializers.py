158d5268678cd488779387cba59548802cd81aa9
Kevin Cook <kevinac4@gmail.com>
2016-07-21 14:09:30 -0700

    needing serializers that are for getting, and others for setting
    and yeah, I know this is wrong

--- back/addressapi/serializers.py
+++ back/addressapi/serializers.py
@@ -3,23 +3,44 @@ from address.models import Address, Locality, State, Country
 from rest_framework import serializers
 
 
-class AddressSerializer(serializers.ModelSerializer):
+class CountrySerializer(serializers.ModelSerializer):
+	country = Country
+	class Meta:
+		model = Country
+
+class StateSerializerGet(serializers.ModelSerializer):
+	state = State
+	country = CountrySerializer(read_only=True)
+	class Meta:
+		model = State
+
+class LocalitySerializerGet(serializers.ModelSerializer):
+	locality = Locality
+	state = StateSerializerGet(read_only=True)
+	class Meta:
+		model = Locality
+
+class AddressSerializerGet(serializers.ModelSerializer):
     address = Address
+    locality = LocalitySerializerGet(read_only=True)
     class Meta:
         model = Address
+
+
+# @todo I don't want to handle this this way, but I also don't have a better solution
+class StateSerializer(serializers.ModelSerializer):
+	state = State
+	class Meta:
+		model = State
 
 class LocalitySerializer(serializers.ModelSerializer):
 	locality = Locality
 	class Meta:
 		model = Locality
 
-class StateSerializer(serializers.ModelSerializer):
-	state = State
-	class Meta:
-		model = State
+class AddressSerializer(serializers.ModelSerializer):
+    address = Address
+    class Meta:
+        model = Address
 
-class CountrySerializer(serializers.ModelSerializer):
-	country = Country
-	class Meta:
-		model = Country
 
